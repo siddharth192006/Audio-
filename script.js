a8e0ad4466a67055c4aa64ef2e7b71a6989a68e1
Siddharth Deshmukh <110730492+siddharth192006@users.noreply.github.com>
2024-08-04 11:30:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+const audio = document.getElementById("audio");
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+const stopButton = document.getElementById("
